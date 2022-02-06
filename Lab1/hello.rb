f320ec42e89346df486ce40427ae30d1be42a08b
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-02-06 14:07:35 +0300

    ex1.4

--- Lab1/hello.rb
+++ Lab1/hello.rb
@@ -1,8 +1,10 @@
+#Задание 1.1-1.2
 if ARGV.length == 1
     puts "Привет, " + ARGV[0]
 else
     puts "Введи правильно: ruby hello.rb имя"
 end
+#Задание 1.3
 language = STDIN.gets.chomp 
 case language
     when "ruby","Ruby", "руби"
@@ -18,3 +20,15 @@ case language
     else
         puts("#{language}? Хм, не знаю такого, но ты всё равно попробуй Ruby")
 end
+#Задание 1.4
+puts "Привет, введи команду ОС:"
+com1 = STDIN.gets
+
+puts "Привет, введи команду Ruby:"
+com2 = STDIN.gets
+
+puts "Команда ОС:"
+system(com1)
+
+puts "Команда Ruby:"
+puts eval(com2)
