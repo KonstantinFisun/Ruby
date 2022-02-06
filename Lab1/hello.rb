02cc65000cf365b78268a57fbe88b1b6a0f1d9e8
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-02-06 14:05:41 +0300

    ex1.3

--- Lab1/hello.rb
+++ Lab1/hello.rb
@@ -3,3 +3,18 @@ if ARGV.length == 1
 else
     puts "Введи правильно: ruby hello.rb имя"
 end
+language = STDIN.gets.chomp 
+case language
+    when "ruby","Ruby", "руби"
+        puts "Выходит, что ты подлиза"
+    when "python","Python", "питон"
+        puts "Python тоже хорош"
+    when "c#","C#", "с#", "сишарп"
+        puts "Неплохой выбор, но мне не зашло"
+    when "java", "Java","джава", "ява"
+        puts "Интересно"
+    when "kotlin","Kotlin", "котлин"
+        puts("Будет полезен")
+    else
+        puts("#{language}? Хм, не знаю такого, но ты всё равно попробуй Ruby")
+end
