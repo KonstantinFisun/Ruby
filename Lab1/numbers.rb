ad7d894269757bab9665e08122e9ff7ca90b68c3
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-02-08 17:18:28 +0300

    ex2.3.3

--- Lab1/numbers.rb
+++ Lab1/numbers.rb
@@ -44,7 +44,7 @@ def pr_numbers(number)
   end
 puts pr
 end
-
+#ex2.3.1
   def top_prime(n) 
       max = n
       lower = 2
@@ -58,7 +58,7 @@ end
       max
       puts max
     end
-
+#ex2.3.2
 def pr_numbersne5(number)
   pr=1
   while number != 0
@@ -67,8 +67,43 @@ def pr_numbersne5(number)
   end
 puts pr
 end
+#ex2.3.3
+require 'prime'
 
-number = ARGV[0].to_i.abs
+def pr_numbers(number)
+  pr=1
+  while number != 0
+    pr*= number % 10
+    number /= 10
+  end
+puts pr
+end 
+def gcd(a, b)
+  if a % b == 0
+    b
+  else
+    gcd(b, a % b)
+  end
+end
+
+arr=[]
+res=[]
+puts 'Введите первое число' 
+one_number = gets.chomp.to_i 
+(1...one_number).each do |i|
+  arr<<i if one_number%i == 0
+end
+puts "Произведение цифр числа"
+pr_numbers(one_number)
+puts"Массив делителей числа"
+print arr
+
+puts "\nПромежуточный результат "
+arr.each { |x| x%2!=0 and (x.prime?)==false and res<<x and puts x}
+puts"Ответ"
+puts gcd(one_number,res.max)
+
+#number = ARGV[0].to_i.abs
 #pr_numbers(number)
 #top_prime(number)
 #pr_numbersne5(number)
