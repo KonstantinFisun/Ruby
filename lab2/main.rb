7259750e9e6e8ea7b41f6b51146ec5a47780cad3
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-05-21 19:49:13 +0300

    Задание 2.1
    Продумайте структуру текстового файла, в котором будут храниться объекты класса Department. Напишите такой файл.
    Вынес в отдельный файл rb класс Department

---
+++ lab2/main.rb
@@ -0,0 +1,24 @@
+
+  def main
+
+
+#Добавление
+person1.duty_add("TRG")
+puts person1
+#Удаление
+ask_duty(person1)
+ person1.delete_duty
+puts  person1
+#Вывести выделенную обязанность
+ person1.just_duty(0)
+puts  person1.text_duty
+#Изменить выделенную обязанность
+ person1.just_post(1)
+ person1.change_dy("Павел")
+end
+
+if __FILE__ == $0
+    main
+end
+end
+
