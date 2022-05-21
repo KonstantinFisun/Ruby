f42cf236be8125b272b4c5a8926787cae662f340
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-05-21 20:13:09 +0300

    Задание 2.2

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,4 +1,22 @@
 
+  reg_name = /^"[\w| |,|.]+/
+  reg_phone = /8\-?([0-9]{3})\-?([ .-]?)([0-9]{3})\2([0-9]{4})/
+  reg_post = /\(["[\w| |,|.|']+"]*\)$/
+  post = []
+  File.open(file,"r") do |f|
+    while (line = f.gets)
+      name = reg_name.match(line).to_s
+      name[0]=''
+      phone = reg_phone.match(line).to_s
+      duty = reg_post.match(line).to_s
+      pp = Department.find_post(duty)
+      pos = Department.new(name, phone)
+      (0..pp.length-1).each { |x| pos.add_post(pp[x]) }
+      post.append(pos)
+    end
+  end
+  post
+
   def main
 list = read_from_txt("Department.txt")
 
