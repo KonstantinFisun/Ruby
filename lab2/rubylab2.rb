d3fdf22bb1086f2f53961f0a734923d5901478d8
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-05-21 18:45:04 +0300

    Задание 1.3
    Создать поле выполнение обязанностей. Реализовать
    методы добавить обязанность, выделить обязанность,
    удалить обязанность, получить текст выделенной
    обязанности, заменить текст выделенной обязанности,
    вывести список обязанностей на экран. Объяснить, почему с точки зрения основных принципов ООП нежелательно получать доступ к массиву целиком.
    Продумать, каким должен быть в этом случае конструктор и внести изменения.

--- lab2/rubylab2.rb
+++ lab2/rubylab2.rb
@@ -1,21 +1,66 @@
 class Department
-  attr_accessor :name,:phone
+  attr_accessor :name, :phone
   
   def initialize (name, phone)
-  @name = name
+    @name = name
+    @duty = duty
     self.phone = phone
   end
   
-   def info
-    puts("#{name} : #{phone}")
+    def to_s
+    "#{@name}, #{@phone}, #{@duty}"
+    "Name: #{@name},\nPhone: #{@phone},\nDuty: #{@duty}\n"
+  end
+  
+  #Метод вывода
+  def print_duty
+    w = ""
+    @duty.each_index { |i| w += "#{i}) #{@duty[i]}\n" }
+    w
+  end
+  
+#Метод добавления обязанности
+  def duty_add(value)
+    @duty.append(value)
+  end
+  
+  #Метод выделения обязанности
+  def just_duty(index)
+    @index = index
+  end
+  
+#Метод удаления обязанности
+  def delete_duty
+    @duty.delete(@post[@index])
+    @index = -1
+  end
+  #Метод получить текст выделенной обязанности
+  def text_duty
+    @duty[@index]
+  end
+  
+#Метод заменить текст выделенной обязанности
+  def change_duty(value)
+    @duty[@index] = value
   end
   
   def main
-  person1 = Department.new("Олеся", "89549270306")
-  person2 = Department.new("Наталья", "89069852101" )
+  person1 = Department.new("Олеся", "89549270306", "Работать целыми сутками")
+  person2 = Department.new("Наталья", "89069852101","Отдыхать" )
 
-  person1.info
-  person2.info
+#Добавление
+person1.duty_add("TRG")
+puts person1
+#Удаление
+ask_duty(person1)
+ person1.delete_duty
+puts  person1
+#Вывести выделенную обязанность
+ person1.just_duty(0)
+puts  person1.text_duty
+#Изменить выделенную обязанность
+ person1.just_post(1)
+ person1.change_dy("Павел")
 end
 
 if __FILE__ == $0
