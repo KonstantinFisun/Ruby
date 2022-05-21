3e511777ddd8edbc6a2a7fdcf8552972aa40a73a
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-05-21 20:12:38 +0300

    Update Department.rb

--- lab2/Department.rb
+++ lab2/Department.rb
@@ -62,22 +62,4 @@ class Department
     is_rus_number?(new_number) ? form_rus_number(new_number): raise(ArgumentError)
   end
   
-def read_from_txt(file)
-  reg_name = /^"[\w| |,|.]+/
-  reg_phone = /8\-?([0-9]{3})\-?([ .-]?)([0-9]{3})\2([0-9]{4})/
-  reg_post = /\(["[\w| |,|.|']+"]*\)$/
-  post = []
-  File.open(file,"r") do |f|
-    while (line = f.gets)
-      name = reg_name.match(line).to_s
-      name[0]=''
-      phone = reg_phone.match(line).to_s
-      duty = reg_post.match(line).to_s
-      pp = Department.find_post(duty)
-      pos = Department.new(name, phone)
-      (0..pp.length-1).each { |x| pos.add_post(pp[x]) }
-      post.append(pos)
-    end
-  end
-  post
-end
+
