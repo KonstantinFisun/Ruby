38199f993625e001efc87b24e8d0916e7da401f6
O_lesya <74838979+Lesya0723@users.noreply.github.com>
2022-05-23 22:56:07 +0300

    Задание 3.2
    Написать метод add_note, добавляющий запись, метод
    choose_note, выделяющий запись, метод change_note, заменяющий
    выбранную запись, метод get_note, возвращающий выбранную запись и
    delete_note, удаляющий выбранную запись.

--- lab2/Dep_list.rb
+++ lab2/Dep_list.rb
@@ -2,7 +2,41 @@ path = File.dirname(__FILE__)
 require "#{path}/Department.rb"
 
 class Department_list
-  def initialize(*list_)
+def initialize(list_)
     @departments = list_
+    @index = -1
+  end
+
+  def add_n(department)
+    @departments.push(department)
+  end
+
+  def choose_n(index)
+    if @departments.length > index and index >= 0 then
+      @index = index
+    else raise ArgumentError.new("Ошибка : Индекс за пределами массива")
+    end
+  end
+
+ 
+  def change_n(department)
+    @departments[@index] = department
+  end
+
+  
+  def get_n
+    @departments[@index]
+  end
+
+  
+  def delete_n
+    @departments.delete_at(@index)
+    @index = -1
+  end
+
+  def to_s
+    s = ""
+    @departments.each_index{|i| s += "Отдел - #{i}\n#{@departments[i]}"}
+    s
   end
 end
